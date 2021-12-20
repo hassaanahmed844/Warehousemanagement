30d2eb44dd538ee8371752c89f00ab3ce2af96cc
Hassaan Ahmed <hassaanahmed844@outlook.com>
2021-12-20 19:28:14 +0500

    n

--- Pages/Items/Create.cshtml.cs
+++ Pages/Items/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Warehousemanagement.Data;
 using Warehousemanagement.Models;
 
@@ -21,6 +22,7 @@ namespace Warehousemanagement.Pages.Items
 
         public IActionResult OnGet()
         {
+            
         ViewData["ProductId"] = new SelectList(_context.Set<Product>(), "Id", "Id");
         ViewData["WearhouseId"] = new SelectList(_context.Set<Warehouse>(), "Id", "Name");
             return Page();
@@ -28,6 +30,13 @@ namespace Warehousemanagement.Pages.Items
 
         [BindProperty]
         public Item Item { get; set; }
+        public IList<Warehouse> AllWarehouse { get; set; }
+        public Warehouse Warehouse { get; set; }
+
+        public IList<Product> AllProducts { get; set; }
+
+        public Product Product { get; set; }
+        public int MyProperty { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -36,9 +45,22 @@ namespace Warehousemanagement.Pages.Items
             {
                 return Page();
             }
+            AllProducts =  _context.Product.ToList();
+            AllWarehouse = _context.Warehouse.ToList();
+            Product = AllProducts.FirstOrDefault(m=> m.Id==Item.ProductId);
+            Warehouse = AllWarehouse.FirstOrDefault(m => m.Id == Item.WearhouseId);
+            if (Product.Length * Product.Width * Product.Height <= Warehouse.Volume)
+            {
+                Warehouse.Volume = Warehouse.Volume - Product.Length * Product.Width * Product.Height;
+                _context.Attach(Warehouse).State = EntityState.Modified;
+                _context.Item.Add(Item);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
 
-            _context.Item.Add(Item);
-            await _context.SaveChangesAsync();
+            }
+
 
             return RedirectToPage("./Index");
         }
