637c3545a78d017909252541f36e44d21f24f5a6
Hassaan Ahmed <hassaanahmed844@outlook.com>
2021-12-27 19:04:45 +0500

    DashBoard Improvement

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,15 +11,24 @@ namespace Warehousemanagement.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Warehousemanagement.Data.WarehousemanagementContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, Warehousemanagement.Data.WarehousemanagementContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
+        [BindProperty]
+        public int ItemCount { get; set; }
+        [BindProperty]
+        public int ProductCount { get; set; }
+        [BindProperty]
+        public int WarehouseCount { get; set; }
         public void OnGet()
         {
-
+            ItemCount = _context.Item.ToList().Count;
+            ProductCount = _context.Product.ToList().Count;
+            WarehouseCount = _context.Warehouse.ToList().Count;
         }
     }
 }
